4bb86802af3e6a21ad3c711a6e19955aec9026ef
faridkotia <v.vansh@iitg.ac.in>
2024-04-23 23:59:51 +0530

    added files

--- comp.sh
+++ comp.sh
@@ -5,5 +5,6 @@ g++ ran.cpp -o ran.exe && ./ran.exe
 
 cd rib_files
 
-for files in output*.rib
-    
+for rib_file in output*.rib; do
+    ../ribosome "$rib_file" "../PDB_files/output${rib_file%.rib}.pdb" ../res.zmat
+done
